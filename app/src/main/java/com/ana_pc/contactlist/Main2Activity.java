50542fc7f8a8a6a032b4fbb4f81f8be58f2afb8e
pereyra-ana <pereyra.ana12@gmail.com>
2017-02-20 01:33:08 -0300

    Fin de primera version. Funcionalidades: alta y modificacion de usuarios. Verificacion de atributo requerido "name" al dar de alta  modificar. Agregado de campos apellido, email, telefono y foto de contacto.

--- app/src/main/java/com/ana_pc/contactlist/Main2Activity.java
+++ app/src/main/java/com/ana_pc/contactlist/Main2Activity.java
@@ -2,12 +2,20 @@ package com.ana_pc.contactlist;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Main2Activity extends Activity {
+
+    private Integer position = null;
+    private static final int REQUEST_CAMERA = 1888;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +30,102 @@ public class Main2Activity extends Activity {
         TextView iconPhoneView = (TextView)findViewById(R.id.icon_phone);
         iconPhoneView.setTypeface(font);
 
+        TextView editUserImage = (TextView)findViewById(R.id.icon_edit);
+        editUserImage.setTypeface(font);
+
+        TextView editImage = (TextView)findViewById(R.id.edit_image);
+        editImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, REQUEST_CAMERA);
+            }
+        });
+
+        Intent intent = this.getIntent();
+        if (intent.getStringExtra("name") != null)
+        {
+            Log.d("CONTACT LIST", "Update action - Nombre: " + intent.getStringExtra("name"));
+            TextView nameText = (TextView)findViewById(R.id.nombre_de_persona);
+            nameText.setText(intent.getStringExtra("name"));
+
+            if (intent.getStringExtra("lastName") != null) {
+                TextView lastNameText = (TextView) findViewById(R.id.lastName);
+                lastNameText.setText(intent.getStringExtra("lastName"));
+            }
+            if (intent.getStringExtra("email") != null) {
+                TextView emailText = (TextView) findViewById(R.id.emailAddress);
+                emailText.setText(intent.getStringExtra("email"));
+            }
+            if (intent.getStringExtra("phone") != null) {
+                TextView phoneText = (TextView) findViewById(R.id.phoneNumber);
+                phoneText.setText(intent.getStringExtra("phone"));
+            }
+            if (intent.getExtras().get("photo") != null) {
+                ImageView image = (ImageView)findViewById(R.id.image_contact);
+                image.setImageBitmap((Bitmap)intent.getExtras().get("photo"));
+            }
+            position = intent.getExtras().getInt("position");
+        }
+        else{
+            Log.d("CONTACT LIST", "Create action");
+        }
+
         findViewById(R.id.save).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                TextView edit = (TextView)findViewById(R.id.nombre_de_persona);
+                TextView nameEdit = (TextView)findViewById(R.id.nombre_de_persona);
+                TextView lastNameEdit = (TextView)findViewById(R.id.lastName);
+                TextView emailEdit = (TextView)findViewById(R.id.emailAddress);
+                TextView phoneEdit = (TextView)findViewById(R.id.phoneNumber);
+                ImageView photo = (ImageView)findViewById(R.id.image_contact);
 
-                String nombre = edit.getText().toString();
+                String name = nameEdit.getText().toString();
+
+                if (name == null || name.isEmpty())
+                {
+                    Toast.makeText(Main2Activity.this, "Name can not be empty", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                String lastName = lastNameEdit.getText().toString();
+                String email = emailEdit.getText().toString();
+                String phone = phoneEdit.getText().toString();
+                Bitmap bitmap = ((BitmapDrawable)photo.getDrawable()) != null ? ((BitmapDrawable)photo.getDrawable()).getBitmap() : null;
 
                 Intent intent2 = new Intent();
+                intent2.putExtra("name", name);
+                intent2.putExtra("lastName", lastName);
+                intent2.putExtra("email", email);
+                intent2.putExtra("phone", phone);
+                intent2.putExtra("photo", bitmap);
 
-                intent2.putExtra("nombre", nombre);
-
-
+                if (position != null)
+                {
+                    intent2.putExtra("action", "update");
+                    intent2.putExtra("position", position);
+                }
+                else
+                {
+                    intent2.putExtra("action", "create");
+                }
                 Main2Activity.this.setResult(RESULT_OK, intent2);
-
                 Main2Activity.this.finish();
-
             }
         });
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CAMERA){
+            if(resultCode == Activity.RESULT_OK)
+            {
+                Bitmap photo = (Bitmap) data.getExtras().get("data");
+                ImageView image = (ImageView)findViewById(R.id.image_contact);
+                image.setImageBitmap(photo);
+            }
+        }
     }
 }
--- app/src/main/java/com/ana_pc/contactlist/MainActivity.java
+++ app/src/main/java/com/ana_pc/contactlist/MainActivity.java
@@ -2,33 +2,70 @@ package com.ana_pc.contactlist;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
-    private String[] nombres = new String[]{
-            "Diego", "Nahuel", "Marcelo", "Agustin", "Diego 2", "Juan", "Manuel", "Ezequiel", "Jonathan",
-            "Juan 2", "Ana Sofia", "Marcos", "Andrea", "Nicolas"
-    };
-
+    private ArrayList<Person> nombres = new ArrayList<>();
     private ListView lista;
+
+    private void loadData()
+    {
+        nombres.add(new Person("Juan 2", null, null, null, null));
+        nombres.add(new Person("Ana Sofia", null, null, null, null));
+        nombres.add(new Person("Marcos", null, null, null, null));
+        nombres.add(new Person("Andrea", null, null, null, null));
+        nombres.add(new Person("Nicolas", null, null, null, null));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        loadData();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         lista = (ListView)findViewById(R.id.lista);
-        lista.setAdapter( miAdapter);
+        lista.setAdapter(miAdapter);
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                Object person = lista.getItemAtPosition(position);
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                intent.putExtra("name", ((Person)person).getName());
+                if (((Person)person).getLastName() != null && !((Person)person).getLastName().isEmpty())
+                {
+                    intent.putExtra("lastName", ((Person)person).getLastName());
+                }
+                if (((Person)person).getEmailAddress() != null && !((Person)person).getEmailAddress().isEmpty())
+                {
+                    intent.putExtra("email", ((Person)person).getEmailAddress());
+                }
+                if (((Person)person).getPhoneNumber() != null && !((Person)person).getPhoneNumber().isEmpty())
+                {
+                    intent.putExtra("phone", ((Person)person).getPhoneNumber());
+                }
+                if (((Person)person).getPhoto() != null)
+                {
+                    intent.putExtra("photo", ((Person)person).getPhoto());
+                }
+                intent.putExtra("position", position);
+                startActivityForResult(intent, 85);
+            }
+        });
+
         findViewById(R.id.add).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,9 +81,23 @@ public class MainActivity extends Activity {
 
         if(requestCode == 85){
             if(resultCode == Activity.RESULT_OK){
-                /*String nombre = data.getStringExtra("nombre");
+                String name = data.getStringExtra("name");
+                String lastName = data.getStringExtra("lastName");
+                String email = data.getStringExtra("email");
+                String phone = data.getStringExtra("phone");
+                Bitmap bitmap = (Bitmap)data.getExtras().get("photo");
 
-                miAdapter.notifyDataSetChanged();*/
+                String action = data.getStringExtra("action");
+                if ("update".equals(action))
+                {
+                    Integer position = data.getExtras().getInt("position");
+                    nombres.get(position).updatePerson(name, lastName, email, phone, bitmap);
+                }
+                else if ("create".equals(action))
+                {
+                    nombres.add(new Person(name, lastName, email, phone, bitmap));
+                }
+                miAdapter.notifyDataSetChanged();
             }
         }
     }
@@ -54,12 +105,12 @@ public class MainActivity extends Activity {
     private BaseAdapter miAdapter = new BaseAdapter() {
         @Override
         public int getCount() {
-            return nombres.length;
+            return nombres.size();
         }
 
         @Override
-        public String getItem(int position) {
-            return nombres[position];
+        public Person getItem(int position) {
+            return nombres.get(position);
         }
 
         @Override
@@ -70,22 +121,21 @@ public class MainActivity extends Activity {
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
 
-            String nombre = getItem(position);
+            String nombre = getItem(position).getName();
+            String lastName = getItem(position).getLastName() != null ? getItem(position).getLastName() : "";
 
             if(convertView == null){
-                Log.d("Lista De Ejemplo", "(" + position + ") Estoy creando un nuevo objeto");
                 LayoutInflater li = getLayoutInflater();
                 convertView = li.inflate(R.layout.row_persona, parent, false);
-            }else{
-                Log.d("Lista De Ejemplo", "(" + position + ") Estoy reciclando un objeto");
             }
 
-            TextView iconView = (TextView) convertView.findViewById(R.id.person_icon);
             Typeface font = Typeface.createFromAsset( getAssets(), "fontawesome-webfont.ttf" );
-            iconView.setTypeface(font);
+
+            TextView userIconView = (TextView) convertView.findViewById(R.id.person_icon);
+            userIconView.setTypeface(font);
 
             TextView nombreView = (TextView) convertView.findViewById(R.id.nombre_de_persona);
-            nombreView.setText(nombre);
+            nombreView.setText(nombre + " " + lastName);
 
             return convertView;
         }
